ec2ca6f6279f891a05773866af36a6b604734003
Diego <diegopena2022@gmail.com>
2022-08-16 16:36:11 -0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-var buttonAdd = document.querySelector('.add')
+// var fiddleBtn = document.querySelector(".add")
 
-var addPlant = function() {
-    
+
+// var addFiddle = function() {
+//     var plant = document.createElement('div')
+//     var plantText = document.createTextNode("Fiddle")
+//     plant.appendChild(plantText);
+//     document.body.appendChild(plant)
+//     plant.classList.add('fiddle')
+// }
+
+// fiddleBtn.addEventListener("click", addFiddle)
+
+var fiddleAdd = document.querySelector("saved-plants")
+var fiddle = "fiddle"
+
+localStorage.setItem("fiddle", fiddle)
+
+if (localStorage = fiddle) {
+    fiddleAdd.innerHTML = "fiddle"
 }
 
-buttonAdd.addEventListener("click", addPlant)
 
